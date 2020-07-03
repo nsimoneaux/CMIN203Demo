3dd5dd6bf39645c2792c885a6543b4470c80892a
nicholas webster simoneaux <nsimoneaux@gmail.com>
2020-07-02 22:50:34 -0500

    first commit
    Testing some stuff

---
+++ hello2.py
@@ -0,0 +1,12 @@
+
+mileage = 8000
+
+loopCount = 0
+
+while (loopCount < 10):
+    mileage = mileage - 1
+    loopCount = loopCount + 1
+    print("Mileage: " + str(mileage))
+    print (loopCount)
+
+print(mileage)
